c6c37191a36257d99ba679d255f89d79fbb0ace5
Elizabeth Babcock <ebabcock@rsmas.miami.edu>
2026-02-05 17:20:05 -0500

    Add functions

--- R/PowerAfterFunctions.r
+++ R/PowerAfterFunctions.r
@@ -5,7 +5,7 @@
 #' measurements at a fixed number of sites before and after a changepoint.
 #'
 #' @param S Number of sites
-#' @param mB  Number of before measurements per site
+#' @param nB  Number of before measurements per site
 #' @param nA Number of after measurements per site
 #' @param delta Hypothesized mean change
 #' @param sd_w Within-site standard deviation
@@ -17,13 +17,13 @@
 #' @returns Estimated power for the given number of after measurements
 #' @export
 #'
-power_for_n_after <- function(S, mB, nA,
+power_for_n_after <- function(S, nB, nA,
                               delta, sd_w, sd_d = 0,
                               alpha = 0.05, nsim = 2000, seed = 1) {
   set.seed(seed)
   pvals <- replicate(nsim, {
     true_change <- rnorm(S, mean = delta, sd = sd_d)
-    yB <- matrix(rnorm(S * mB, mean = 0, sd = sd_w), nrow = S)
+    yB <- matrix(rnorm(S * nB, mean = 0, sd = sd_w), nrow = S)
     yA <- matrix(rnorm(S * nA, mean = true_change, sd = sd_w), nrow = S)
     D <- rowMeans(yA) - rowMeans(yB)
     t.test(D, mu = 0)$p.value
@@ -37,7 +37,7 @@ power_for_n_after <- function(S, mB, nA,
 #' power, for a paired t-test on the before and after means.
 #'
 #' @param S Number of sites
-#' @param mB Number of before measurements per site
+#' @param nB Number of before measurements per site
 #' @param delta Hypothesized mean change
 #' @param sd_w Within-site standard deviation
 #' @param sd_d Between-site standard deviation of true changes
@@ -50,14 +50,14 @@ power_for_n_after <- function(S, mB, nA,
 #' @returns A list containing the minimum number of after measurements needed to achieve the target power and a data frame of the power curve
 #' @export
 #'
-find_n_after <- function(S, mB,
+find_n_after <- function(S, nB,
                          delta, sd_w, sd_d = 0,
                          target_power = 0.8,
                          alpha = 0.05,
                          n_grid = 1:50,
                          nsim = 2000, seed = 1) {
   pow <- sapply(n_grid, function(nA) {
-    power_for_n_after(S = S, mB = mB, nA = nA,
+    power_for_n_after(S = S, nB = nB, nA = nA,
                       delta = delta, sd_w = sd_w, sd_d = sd_d,
                       alpha = alpha, nsim = nsim, seed = seed)
   })
@@ -72,7 +72,7 @@ find_n_after <- function(S, mB,
 #' of sites before and after a changepoint.
 #'
 #' @param S Number of sites
-#' @param mB Number of before measurements per site
+#' @param nB Number of before measurements per site
 #' @param nA Number of after measurements per site
 #' @param delta Hypothesized mean change
 #' @param sd_w Within-site standard deviation
@@ -84,10 +84,10 @@ find_n_after <- function(S, mB,
 #' @returns Estimated power for the given number of sites
 #' @export
 #'
-power_for_sites <- function(S, mB, nA,
+power_for_sites <- function(S, nB, nA,
                             delta, sd_w, sd_d = 0,
                             alpha = 0.05, nsim = 2000, seed = 1) {
-  power_for_n_after(S = S, mB = mB, nA = nA,
+  power_for_n_after(S = S, nB = nB, nA = nA,
                     delta = delta, sd_w = sd_w, sd_d = sd_d,
                     alpha = alpha, nsim = nsim, seed = seed)
 }
@@ -97,7 +97,7 @@ power_for_sites <- function(S, mB, nA,
 #' Function to find the minimum number of sites needed to achieve target power from
 #' a paired t test on the means across observations at sites before and after a changepoint.
 #'
-#' @param mB Number of before measurements per site
+#' @param nB Number of before measurements per site
 #' @param nA Number of after measurements per site
 #' @param delta Hypothesized mean change
 #' @param sd_w Within-site standard deviation
@@ -119,7 +119,7 @@ find_min_sites <- function(mB, nA,
                            nsim = 2000, seed = 1) {
   S_grid <- S_grid[S_grid >= 2]
   pow <- sapply(S_grid, function(S) {
-    power_for_sites(S = S, mB = mB, nA = nA,
+    power_for_sites(S = S, nB = nB, nA = nA,
                     delta = delta, sd_w = sd_w, sd_d = sd_d,
                     alpha = alpha, nsim = nsim, seed = seed)
   })
@@ -148,3 +148,72 @@ getSD_within <- function(baseline,siteVar="site",responseVar="y"){
   return(sd_within_hat)
 }
 
+#' getSD_difference
+#'
+#' Estimate standard deviation of difference in means before and after change for
+#' paired t-test of difference in means.
+#'
+#' @param sd_within standard deviation within sites
+#' @param nA number of after samples per site
+#' @param nB number of before samples per site
+#' @param sd_delta standard deviation of true changes among sites
+#'
+#' @returns Standard deviation of the difference in means
+#'
+#' @examples
+getSD_difference <- function(sd_within, nA, nB, sd_delta){
+  sd_diff <- sqrt(sd_within^2*(1/nA + 1/nB) + sd_delta^2)
+  return(sd_diff)
+}
+
+#' find_min_sites_analytical
+#'
+#' Function to use power.t.test to calculate power for a paired t-test on the means across
+#'
+#' @param n_grid Number of sites grid
+#' @param delta_target Target mean change
+#' @param sd_diff Standard deviation of difference in means
+#'
+#' @returns Power for each number of sites in n_grid
+#' @export
+#'
+find_min_sites_analytical <- function(S_grid, delta_target, sd_diff){
+  S_grid <- S_grid[S_grid >=2]
+  sapply(S_grid, function(n) {
+    power.t.test(
+      n = n,
+      delta = delta_target,
+      sd = sd_diff,
+      sig.level = 0.05,
+      type = "one.sample",
+      alternative = "two.sided"
+    )$power
+  } )
+}
+
+#' power_for_nA
+#' Function to calculate power for given number of after samples for a paire t-test
+#' of the difference in means before vs. after change using power.t.test
+#'
+#' @param nA Number of after samples per site
+#' @param S Number of sites
+#' @param nB Number of before samples per site
+#' @param delta Hypothesized mean change
+#' @param sd_within Standard deviation within sites
+#' @param sd_delta Standard deviation of true changes among sites
+#' @param alpha Significance level
+#'
+#' @returns Power for the given number of after sample
+#' @export
+#'
+power_for_nA <- function(nA, S, nB, delta, sd_within, sd_delta, alpha){
+  sd_diff <- getSD_difference(sd_within, nA, nB, sd_delta)
+  power_result <- power.t.test(n = S,
+                               delta = delta,
+                               sd = sd_diff,
+                               sig.level = alpha,
+                               power = NULL,
+                               type = "paired",
+                               alternative = "two.sided")
+  return(power_result$power)
+}
