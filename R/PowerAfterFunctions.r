54c67a3ce64d1c1f81a9a209e9501ba606894c2a
Elizabeth Babcock <ebabcock@rsmas.miami.edu>
2026-02-22 16:17:15 -0500

    Add power_for_percent_change

--- R/PowerAfterFunctions.r
+++ R/PowerAfterFunctions.r
@@ -444,3 +444,60 @@ find_min_detectable_percent <- function(S,
   return(percent_change)
 }
 
+#' power_for_percent_change
+#'
+#' Function to calculate power for a given percent change in the mean
+#' before vs. after change using power.t.test (paired t-test on site-level differences).
+#'
+#' @param percent_change Target percent change in the original response variable
+#' @param baseline_mean Mean of the original response variable before the change
+#' @param nA Number of after samples per site
+#' @param S Number of sites
+#' @param nB Number of before samples per site
+#' @param sd_within Standard deviation within sites (log calculated if logTransform=TRUE)
+#' @param sd_delta Standard deviation of true changes among sites
+#' @param alpha Significance level
+#' @param logTransform Logical indicating whether to work on log(y + logAdd) scale (default FALSE)
+#' @param logAdd Value to add to response variable before log-transforming to avoid issues
+#'
+#' @returns Power for the given percent change
+#' @export
+power_for_percent_change <- function(percent_change,
+                                     baseline_mean,
+                                     nA,
+                                     S,
+                                     nB,
+                                     sd_within,
+                                     sd_delta,
+                                     alpha,
+                                     logTransform = FALSE,
+                                     logAdd = 0) {
+  if (is.null(baseline_mean)) {
+    stop("baseline_mean must be provided.")
+  }
+  if (baseline_mean <= 0) {
+    stop("baseline_mean must be > 0.")
+  }
+
+  if (logTransform) {
+    before <- baseline_mean
+    after <- baseline_mean * (1 + percent_change / 100)
+    delta <- log(after + logAdd) - log(before + logAdd)
+  } else {
+    delta <- baseline_mean * (percent_change / 100)
+  }
+
+  sd_diff <- getSD_difference(sd_within, nA, nB, sd_delta)
+
+  power_result <- power.t.test(
+    n = S,
+    delta = delta,
+    sd = sd_diff,
+    sig.level = alpha,
+    power = NULL,
+    type = "paired",
+    alternative = "two.sided"
+  )
+
+  return(power_result$power)
+}
